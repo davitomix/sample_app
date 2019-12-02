12c1cf02c286ffedbbd289ba7777f7d3198a608e
David Elì <davidelimg1@gmail.com>
2019-12-02 14:04:29 -0600

    Finish static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   get 'foo/bar'
   get 'foo/baz'
+  root 'static_pages#home'
   get 'static_pages/home'
   get 'static_pages/help'
   get  'static_pages/about'
-  root 'application#hello' 
+  get  'static_pages/contact'
+  root 'static_pages#home' 
 end
