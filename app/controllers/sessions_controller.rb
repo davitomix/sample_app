3848e8705bbea879caecdcfc022c79c58e4e63df
David Elì <davidelimg1@gmail.com>
2019-12-10 14:48:28 -0600

    Add test integration to login form, and alert messages

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,8 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:session][:password])
       # Log the user in and redirect to the user's show page.
     else
-      # Create an error message.
+      # Create an error message dissappear as soons as theres another request.
+      flash.now[:danger] = 'Invalid email/password combination'
       render 'new'
     end
   end
