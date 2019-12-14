939db509ee76328c06c9fc754ba5f5258b091502
David Elì <davidelimg1@gmail.com>
2019-12-13 20:25:00 -0600

    Added test for admin users

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -11,7 +11,7 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     get users_path
     assert_redirected_to login_url
   end
-  
+
   test "should get new" do
     get signup_path
     assert_response :success
@@ -28,5 +28,15 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
                                               email: @user.email } }
     assert_not flash.empty?
     assert_redirected_to login_url
+  end
+
+  test "should not allow the admin attribute to be edited via the web" do
+    log_in_as(@other_user)
+    assert_not @other_user.admin?
+    patch user_path(@other_user), params: {
+                                    user: { password:              '',
+                                            password_confirmation: '',
+                                            admin: true } }
+    assert_not @other_user.admin?
   end
 end
